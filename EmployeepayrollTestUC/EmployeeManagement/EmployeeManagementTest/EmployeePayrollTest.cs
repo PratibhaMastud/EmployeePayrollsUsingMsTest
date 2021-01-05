b75b20138189d18d49fd573daa9d53f664799708
PratibhaMastud <karandepratibha999@gmail.com>
2021-01-05 23:35:27 +0530

    [Pratibha Mastud] Add : Add New Employee in database and list

--- EmployeepayrollTestUC/EmployeeManagement/EmployeeManagementTest/EmployeePayrollTest.cs
+++ EmployeepayrollTestUC/EmployeeManagement/EmployeeManagementTest/EmployeePayrollTest.cs
@@ -9,7 +9,7 @@ namespace EmployeeManagementTest
     public class EmployeePayrollTest
     {
         [TestMethod]
-        public void Given10Employee_WhenAddedList_ShouldMatchWithEmployeeEntries()
+        public List<SalaryDetailsModel> Given10Employee_WhenAddedList_ShouldMatchWithEmployeeEntries()
         {
             List<SalaryDetailsModel> employeeDetails = new List<SalaryDetailsModel>();
             employeeDetails.Add(new SalaryDetailsModel( EmployeeId: 1, EmployeeName: "Ankita", JobDiscription: "IT", Month: "jan", EmployeeSalary: 24000, SalaryId: 7, date: new DateTime(2019 - 10 - 11), gender: 'F'));
@@ -22,7 +22,7 @@ namespace EmployeeManagementTest
             employeeDetails.Add(new SalaryDetailsModel(EmployeeId: 8, EmployeeName: "prasad", JobDiscription: "Comp", Month: "Feb", EmployeeSalary: 40000, SalaryId: 6, date: new DateTime(2021 - 11 - 5), gender: 'F'));
             employeeDetails.Add(new SalaryDetailsModel(EmployeeId: 9, EmployeeName: "supu", JobDiscription: "EXTC", Month: "jan", EmployeeSalary: 40000, SalaryId: 5, date: new DateTime(2010 - 5 - 16), gender: 'M'));
             employeeDetails.Add(new SalaryDetailsModel(EmployeeId: 10, EmployeeName: "chetna", JobDiscription: "EXTC", Month: "jan", EmployeeSalary: 44000, SalaryId: 7, date: new DateTime(2011 - 12 - 25), gender: 'F'));
-
+           
             EmployeePayrollOperation employeePayrollOperation = new EmployeePayrollOperation();
             /*DateTime statDateTime = DateTime.Now;
             employeePayrollOperation.addEmployeePayroll(employeeDetails);
@@ -32,13 +32,13 @@ namespace EmployeeManagementTest
             Salary salary = new Salary();
             SalaryDetailsModel model = new SalaryDetailsModel()
             {
-                EmployeeId = 17,
-                EmployeeName = "rajesh",
+                EmployeeId = 6,
+                EmployeeName = "Vaibhav",
                 JobDiscription = "EXTC",
                 Month = "March",
                 EmployeeSalary = 50000.00,
-                SalaryId = 22,
-                date = new DateTime(2019, 2, 1),
+                SalaryId = 20,
+                date = new DateTime(2015, 2, 1),
                 gender = 'M'
 
             };
@@ -53,13 +53,44 @@ namespace EmployeeManagementTest
             salary.addEmployee(model); 
             DateTime stopTime1 = DateTime.Now; 
             Console.WriteLine("Duration without thread = " + (stopTime1 - startTime1));
-
+            Console.WriteLine("Execution time is :" + (stopTime1 - startTime1));
 
             DateTime startTimeWithThread = DateTime.Now;
             employeePayrollOperation.addEmployeeWithThread(employeeDetails);
             DateTime endTimeWithThread = DateTime.Now;
             Console.WriteLine("Duration with thread = " + (startTimeWithThread - endTimeWithThread));
+            Console.WriteLine("Execution time is :" + (startTimeWithThread - endTimeWithThread));
+            return employeeDetails;
+        }
 
+        [TestMethod]
+        public void AddMultipleRecord()
+        {
+            Salary salary = new Salary();
+            SalaryDetailsModel employeeModel = new SalaryDetailsModel
+            {
+
+                EmployeeId = 25,
+                EmployeeName = "Aarti",
+                JobDiscription = "IT",
+                Month = "March",
+                EmployeeSalary = 80000.00,
+                SalaryId = 22,
+                date = new DateTime(2021, 2, 1),
+                gender = 'M'
+            };
+
+            DateTime startTimes = DateTime.Now;
+            salary.addEmployee(employeeModel);
+            DateTime endTimes = DateTime.Now;
+            Console.WriteLine("Duration without thread = " + (endTimes - startTimes));
+
+            List<SalaryDetailsModel> list =  Given10Employee_WhenAddedList_ShouldMatchWithEmployeeEntries();
+            EmployeePayrollOperation employeePayrollOperation = new EmployeePayrollOperation();
+            DateTime startTimeWithThread = DateTime.Now;
+            employeePayrollOperation.addEmployeeWithThread(list);
+            DateTime endTimeWithThread = DateTime.Now;
+            Console.WriteLine("Duration with thread = " + (startTimeWithThread - endTimeWithThread));
         }
     }
 }
